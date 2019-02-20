375a5ae6ddb19c6101e9396a444f00b9b484b1fa
dicky-chandra <m.dicky2903@gmail.com>
2019-02-20 21:32:59 +0700

    tugas saya

---
+++ LATIHAN 2.py
@@ -0,0 +1,10 @@
+print("*****LATIHAN 2*****")
+print("masukan bilangan, berhenti ketika bilangan 0, dan menampilkan bilangan terbesar")
+
+b=0
+while True:
+    angka = int(input("masukan bilangan"))
+    b = angka
+    if b == 0:
+       break
+print("******SELESAI******")
